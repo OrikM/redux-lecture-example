f93b90b23a8fd3278f71b58242dc5b18f69cfac1
Orkhan <omehraliyev@gmail.com>
2023-07-12 01:13:34 +0400

    final-version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import Goods from './components/Goods';
+import Cart from './components/Cart';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          <Header />
+        </header>
+        <main class="app__main">
+          <div class="app__goods">
+            <Goods />
+          </div>
+          <div class="app__cart">
+            <Cart />
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
